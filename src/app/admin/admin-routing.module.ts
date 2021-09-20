114b6b666e0ceb3a2d88ac7c0bfc335c1435cd9a
Kevin Garcia <developer.kevin.95@gmail.com>
2021-09-20 15:45:55 -0600

    listar productos admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NavComponent,
-    children:[{
+    children:[
+      {
       path: 'create',
       component: ProductFormComponent      
-    }]
+    },
+    {
+      path: 'products',
+      component: ProductsListComponent      
+    }
+
+  ]
   }
 
 ];
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,33 +1,27 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './../material/material.module';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material/material.module';
 import { NavComponent } from './components/nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+
+
 
 
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent],
+  declarations: [
+    ProductFormComponent, 
+    NavComponent, 
+    ProductsListComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
     ReactiveFormsModule,
-    MaterialModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    MaterialModule
   ]
 })
 export class AdminModule { }
